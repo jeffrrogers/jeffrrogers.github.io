7bda1850a3cfec2578365e7aef6361c832614a8d
Jeff Rogers <jeffr@ca.ibm.com>
2022-09-19 23:53:19 -0400

    - Did a flutter clean and build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e8298032d534fe9d3c524f4aede9f958",
-"index.html": "70331e390bf2830c2abbb8d2e62e2630",
-"/": "70331e390bf2830c2abbb8d2e62e2630",
+"index.html": "1b9dfd1a79a319d0d13eea62d68a9bb5",
+"/": "1b9dfd1a79a319d0d13eea62d68a9bb5",
 "main.dart.js": "0d5043e9639ccb4e036b9f4c5938901e",
+"flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -26,7 +27,11 @@ const RESOURCES = {
 "assets/assets/twitter.png": "53a82498778b43cf389c7a236bf8f1b3",
 "assets/assets/delete.png": "250c4241c21bed03a57bf740aa66e567",
 "assets/assets/leaf.png": "76fccd9aec5701e7fd4de1b98182d1da",
-"assets/assets/facebook.png": "5d6ea38a769498dfc19fe6389d14db39"
+"assets/assets/facebook.png": "5d6ea38a769498dfc19fe6389d14db39",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
 };
 
 // The application shell files that are downloaded before a service worker can
