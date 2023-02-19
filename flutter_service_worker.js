1372393440e61c8393287ab62722d489e1537a5b
Jeff Rogers <jeffr@ca.ibm.com>
2023-02-18 23:22:45 -0500

    - Testing moving words/answers/funFacts external from the app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e5f05d437b7326ef727c20f4fc3bf9b7",
-"index.html": "a71f7c25cefb019798382c2b492e6fcf",
-"/": "a71f7c25cefb019798382c2b492e6fcf",
-"main.dart.js": "101cc7cac28baced154238b060cadcdc",
+"index.html": "d70e9f5647f73ae42df02aeba4e39123",
+"/": "d70e9f5647f73ae42df02aeba4e39123",
+"main.dart.js": "4e71653db93b1ee6de01cb6b9d2ec050",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
