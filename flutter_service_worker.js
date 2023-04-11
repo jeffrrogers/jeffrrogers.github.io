5bb45f98a2db9dffcf334ace47aa761abd381074
Jeff Rogers <jeffr@ca.ibm.com>
2023-04-10 22:55:10 -0400

    Fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bfc86c2689c0c47aef2a9336399bbb77",
-"index.html": "9ad6d385e9f9d840419a27767a2274d6",
-"/": "9ad6d385e9f9d840419a27767a2274d6",
-"main.dart.js": "72fd5e61b47ebdfa182207d2c0bdbe70",
+"index.html": "e7dd7e12f30e20d575c095d7d80b2f63",
+"/": "e7dd7e12f30e20d575c095d7d80b2f63",
+"main.dart.js": "4a0d832b08f334ad0c00d657d49ad6aa",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
