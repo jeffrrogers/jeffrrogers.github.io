51f3e4fed722cae625ae65bf973b5b00b62ee097
Jeff Rogers <jeffr@ca.ibm.com>
2024-10-20 23:27:24 -0400

    - Tweaks to stat wording/spacing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ae6ecfd120fc06307b3d00d093ac8052",
+const RESOURCES = {"flutter_bootstrap.js": "6d7cb0bb90b3ebd35aaefeaa6e43bed6",
 "version.json": "e51b1e36b44581f8113aafdacf9a125f",
 "index.html": "e00da3d62b49febbb01d4f60399f6bef",
 "/": "e00da3d62b49febbb01d4f60399f6bef",
-"main.dart.js": "bef061f0e80b1d03bb6eb529686e5246",
+"main.dart.js": "c8537e4a4a0dcd583984fccd8fc80822",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
