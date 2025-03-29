30e4c6bd96cffdac9d3b1222327648e4f3bb9e06
Jeff Rogers <jeffr@ca.ibm.com>
2025-03-29 00:01:16 -0400

    Canuckle+ updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5d9d5f9b07791627d566b8c95c46eebb",
+const RESOURCES = {"flutter_bootstrap.js": "feeadb093fe530a9b9c643481fbaf3ee",
 "version.json": "0f49f41395ab4f03e13d1da5ea910ef4",
 "index.html": "af47124368931df7fb04b7ae70cb1ab3",
 "/": "af47124368931df7fb04b7ae70cb1ab3",
-"main.dart.js": "e4c993d86aaace6666884a342f43a054",
+"main.dart.js": "180678910ca4c4b1c1fd3c9326865fcd",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
