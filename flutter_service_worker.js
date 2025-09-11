f40c44ab616e8f3c56736a8680e9f1e92120f819
Jeff Rogers <jeffr@ca.ibm.com>
2025-09-11 15:13:41 -0400

    -Removed canuckle game # from version text in settings

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "174443b1f9be1aa5619198e047fa62a6",
+const RESOURCES = {"flutter_bootstrap.js": "8a425952c6be73fe9e9e167fccea7eb6",
 "version.json": "0e48dfb64568c930f36785442843b604",
 "index.html": "af47124368931df7fb04b7ae70cb1ab3",
 "/": "af47124368931df7fb04b7ae70cb1ab3",
-"main.dart.js": "c697f68c1344d1e3e0ab905d3216174d",
+"main.dart.js": "8f1c85182e1b72b92cc6a3924726cba7",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
