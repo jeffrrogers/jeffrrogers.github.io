71be62e7d7ce9b70c29e463f6a7a569d0abfdea2
Jeff Rogers <jeffr@ca.ibm.com>
2025-11-04 09:54:09 -0500

    - Fixed issue with non existent chars

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6f8af35f1edb1f10488250a6afbc2882",
+const RESOURCES = {"flutter_bootstrap.js": "b952d2f2e249ac652d82cbbac8a8f26a",
 "version.json": "0e48dfb64568c930f36785442843b604",
 "index.html": "af47124368931df7fb04b7ae70cb1ab3",
 "/": "af47124368931df7fb04b7ae70cb1ab3",
-"main.dart.js": "7c0423560ca3295e00a86cbfafe31ce2",
+"main.dart.js": "df4a12f6350030b3cf5a9ae6449aafea",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
