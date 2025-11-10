a700d61c698a7f14d962c7045aa2164a6909bbaf
Jeff Rogers <jeffr@ca.ibm.com>
2025-11-09 22:06:44 -0500

    - Fixed issue loading canuckle+ answers for archive

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a193f2ad686a1e9bed848256a609ca7c",
+const RESOURCES = {"flutter_bootstrap.js": "faa05aabb7de22cb867a591f1996a0e5",
 "version.json": "0e48dfb64568c930f36785442843b604",
 "index.html": "af47124368931df7fb04b7ae70cb1ab3",
 "/": "af47124368931df7fb04b7ae70cb1ab3",
-"main.dart.js": "308ce93427877c82722cde4d7601c34d",
+"main.dart.js": "89a392265e96341cc16088b89502f69d",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
