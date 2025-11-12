bc4f312c07498c94001f942b3172c9580ce63093
Jeff Rogers <jeffr@ca.ibm.com>
2025-11-12 15:40:58 -0500

    hard mode fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8d6e45d54805c78373708313cca076dc",
-"version.json": "0e48dfb64568c930f36785442843b604",
+const RESOURCES = {"flutter_bootstrap.js": "32839791f699a8ab528166ee15503f79",
+"version.json": "be7f684beed031e016f65755fd42d4b6",
 "index.html": "af47124368931df7fb04b7ae70cb1ab3",
 "/": "af47124368931df7fb04b7ae70cb1ab3",
-"main.dart.js": "9b68d09f54c6c4ff88b587c381144b0d",
+"main.dart.js": "25373c1ed126175a69d80ad570507d06",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
