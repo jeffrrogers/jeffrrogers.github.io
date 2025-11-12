7c87dd04b279298242984ec4f8a11a61cfa23e28
Jeff Rogers <jeffr@ca.ibm.com>
2025-11-11 22:06:48 -0500

    - fix red dot check with daylight savings

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ee1f5f6e07edc53337602ba994cfb2a5",
+const RESOURCES = {"flutter_bootstrap.js": "c32b98776096ccaea8790212617da3a8",
 "version.json": "0e48dfb64568c930f36785442843b604",
 "index.html": "af47124368931df7fb04b7ae70cb1ab3",
 "/": "af47124368931df7fb04b7ae70cb1ab3",
-"main.dart.js": "9685350eae92f0e5d949eff1441fea37",
+"main.dart.js": "b5364b75fbdbb9d270b8fd7f0d4c227d",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
