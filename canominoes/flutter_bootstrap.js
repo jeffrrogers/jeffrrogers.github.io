7c4b84bef8e881d4bdb60dd58a795d23b2843de8
jeffrrogers <jrog@rogers.com>
2026-09-02 23:38:51 -0400

    Updates

--- canominoes/flutter_bootstrap.js
+++ canominoes/flutter_bootstrap.js
@@ -47,7 +47,7 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a362","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=202609022302"},{}]};
+_flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a362","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=202609022335"},{}]};
 
 
 // -- Service worker ---------------------------------------------------------
